f1ef84217bfe87f3b0c4beee7d1a747f7b7f3f64
kazukin <kazukin@gmail.com>
2026-02-21 01:14:34 +0900

    バックエンド設定(TF32)を追加。

--- core/anet-core/include/anet/init.hpp
+++ core/anet-core/include/anet/init.hpp
@@ -1,8 +1,27 @@
 ﻿// anet/init.hpp
+
 #pragma once
+
+#include "anet/config.hpp"
 
 namespace anet::rl {
 
-	void InitRL();
+	struct BackendConfig : public anet::Config {
+        bool use_tf32_cublas = true;
+        bool use_tf32_cudnn = true;
+        bool cudnn_deterministic = false;
+        bool cudnn_benchmark = false;
 
+        explicit BackendConfig(const ConfigData& config_data = EmptyConfigData, const std::string& prefix = "backend")
+            : anet::Config(config_data, prefix)
+        {
+            ANET_READ_CONFIG(config_data, use_tf32_cublas);
+            ANET_READ_CONFIG(config_data, use_tf32_cudnn);
+            ANET_READ_CONFIG(config_data, cudnn_deterministic);
+            ANET_READ_CONFIG(config_data, cudnn_benchmark);
+        }
+	};
+
+	void InitRL(const BackendConfig& backend_config);
 }
+
--- core/anet-core/src/init.cpp
+++ core/anet-core/src/init.cpp
@@ -6,8 +6,15 @@
 
 using namespace anet::rl;
 
-void anet::rl::InitRL()
+void anet::rl::InitRL(const BackendConfig& backend_config)
 {
+	// Torch関連設定反映
+	torch::Context& ctx = torch::globalContext();
+	ctx.setAllowTF32CuBLAS(backend_config.use_tf32_cublas);
+	ctx.setAllowTF32CuDNN(backend_config.use_tf32_cudnn);
+	ctx.setDeterministicCuDNN(backend_config.cudnn_deterministic);  // 非決定論的である代わりに高速化
+	ctx.setBenchmarkCuDNN(backend_config.cudnn_benchmark);		// サイズが変化しない場合に高速化
+
 	anet::nn::InitNN();
 	RegisterAgentFactory<DQNAgentFactory>();
 	RegisterAgentFactory<anet::rl::dqn::RainbowAgentFactory>();
