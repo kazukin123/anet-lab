7e06269d0dac891f5861e506e945ca45e99c75c3
kazukin123 <kazukin@gmail.com>
2026-09-17 02:20:16 +0900

    docs(Atari): replayのlane窓とevalコストの実験記録を追加 #22

--- apps/11_batch_run.bat
+++ apps/11_batch_run.bat
@@ -4,6 +4,13 @@ cd /d "%~dp0runner"
 
 SET "BUILD=RelWithDebInfo"
 
+:waitprev
+tasklist /FI "IMAGENAME eq AnetRLRunner_ab.exe" 2>nul | find /I "AnetRLRunner_ab.exe" >nul
+if not errorlevel 1 (
+  ping -n 31 127.0.0.1 >nul
+  goto :waitprev
+)
+
 if not exist "bin\%BUILD%\AnetRLRunner.exe" goto :no_exe
 copy /Y "bin\%BUILD%\AnetRLRunner.exe" "bin\%BUILD%\AnetRLRunner_ab.exe" >nul
 if errorlevel 1 goto :no_exe
@@ -19,17 +26,21 @@ SET "A5=run.@v5_iqn_impala_x2>run.@a5>run.@a5_apex>run.@va_base"
 SET "RR4=run.@hard500>run.@rr4>run.@munch"
 SET "RF=run.@rfit"
 SET "NET=run.@btrnet>run.@btrsn"
+SET "EV=run.@evalonly>run.@eval2only>run.@greedy_eval>run.@to_50"
+SET "TP=run.@evaloff>run.@to_10m"
 
-echo === 1. RR4 + Pool4 trunk SN 20M (4h) ===
-call :run_exe "run.$=%A5%>%RR4%>%RF%>run.@btrtrunkpool4>run.@envs64>run.@a5_20m>run.@btreval_r4" "app.run_name=run_{t}_rr4_btrtrunkpool4_20m"
-echo === 2. RR4 + BTR net + SN, num_envs 128, 50M (8.5h) ===
-call :run_exe "run.$=%A5%>%RR4%>%RF%>%NET%>run.@btreval_r4" "app.run_name=run_{t}_rr4_btrfull_envs128_50m"
-echo === 3. walltime preset: RR1 + kyu-net 100M (7-8h) ===
-call :run_exe "run.$=%A5%>run.@hard125>run.@munch>%RF%>run.@to_100m>run.@btreval_r1" "app.run_name=run_{t}_rr1_va_hard125_100m"
-echo === 4. RR4 + Pool3 trunk SN 20M (4h) ===
-call :run_exe "run.$=%A5%>%RR4%>%RF%>run.@btrtrunkpool3>run.@envs64>run.@a5_20m>run.@btreval_r4" "app.run_name=run_{t}_rr4_btrtrunkpool3_20m"
-echo === 5. RR4 + BTR net + SN + actor_approx 20M (3.5h) ===
-call :run_exe "run.$=%A5%>%RR4%>%RF%>%NET%>run.@aainit>run.@envs64>run.@a5_20m>run.@btreval_r4" "app.run_name=run_{t}_rr4_btrfull_aainit_20m"
+SET "S_CAP2M=workspaces/atari-3rd/runs/run_20260915-203706_rr4_btrfull_envs128_cap2m_50m/agent_close.anet"
+
+echo === 1. ev: envs128 cap2M 50M eps=0 (12m) ===
+call :run_exe "run.$=%A5%>run.@munch>%NET%>%EV%" "A3.auto_load_file=%S_CAP2M%" "app.run_name=run_{t}_ev_btrfull_envs128_cap2m_50m"
+echo === 2. throughput: RR1 kyu-net, eval off, 10M (0.5h) ===
+call :run_exe "run.$=%A5%>run.@hard125>run.@munch>%RF%>%TP%" "app.run_name=run_{t}_tp_rr1_va_evaloff_10m"
+echo === 3. throughput: RR4 btrfull SN 15/15, eval off, 10M (1.5h) ===
+call :run_exe "run.$=%A5%>%RR4%>%RF%>%NET%>run.@envs64>%TP%" "app.run_name=run_{t}_tp_rr4_btrfull_evaloff_10m"
+echo === 4. throughput: RR4 SN 12/15, eval off, 10M (1.5h) ===
+call :run_exe "run.$=%A5%>%RR4%>%RF%>run.@btrnet>run.@btrsn12>run.@envs64>%TP%" "app.run_name=run_{t}_tp_rr4_sn12_evaloff_10m"
+echo === 5. RR4 + BTR net + SN, envs64 + cap2M (lane 32,768) 50M (10h) ===
+call :run_exe "run.$=%A5%>%RR4%>%RF%>%NET%>run.@cap2m>run.@envs64>run.@btreval_r4" "app.run_name=run_{t}_rr4_btrfull_envs64_cap2m_50m"
 
 if "%FAILED_RUNS%"=="0" goto :all_succeeded
 echo === ALL DONE: %SUCCEEDED_RUNS% SUCCEEDED, %FAILED_RUNS% FAILED ===
--- apps/12_batch_run.bat
+++ apps/12_batch_run.bat
@@ -4,6 +4,13 @@ cd /d "%~dp0runner"
 
 SET "BUILD=RelWithDebInfo"
 
+:waitprev
+tasklist /FI "IMAGENAME eq AnetRLRunner_ab.exe" 2>nul | find /I "AnetRLRunner_ab.exe" >nul
+if not errorlevel 1 (
+  ping -n 31 127.0.0.1 >nul
+  goto :waitprev
+)
+
 if not exist "bin\%BUILD%\AnetRLRunner.exe" goto :no_exe
 copy /Y "bin\%BUILD%\AnetRLRunner.exe" "bin\%BUILD%\AnetRLRunner_ab.exe" >nul
 if errorlevel 1 goto :no_exe
@@ -16,22 +23,28 @@ SET /A FAILED_RUNS=0
 SET "FIX2=E1.game=breakout"
 
 SET "A5=run.@v5_iqn_impala_x2>run.@a5>run.@a5_apex>run.@va_base"
+SET "RR4=run.@hard500>run.@rr4>run.@munch"
 SET "RF=run.@rfit"
 SET "NET=run.@btrnet>run.@btrsn"
-SET "NOSN=workspaces/atari-3rd/runs/run_20260913-072109_rr4_btrnosn_resume30m/agent_close.anet"
+SET "EV=run.@evalonly>run.@eval2only>run.@greedy_eval>run.@to_50"
 
-echo === 1. ev: btrnosn 50M close eval2 greedy (20m) ===
-call :run_exe "run.$=%A5%>run.@munch>run.@btrnet>run.@evalonly>run.@eval2only>run.@greedy_eval>run.@to_50" "A3.auto_load_file=%NOSN%" "app.run_name=run_{t}_ev_btrnosn_50m"
-echo === 2. RR1 + BTR net + SN 50M (3.5h) ===
-call :run_exe "run.$=%A5%>run.@hard125>run.@munch>%RF%>%NET%>run.@envs64>run.@btreval_r1" "app.run_name=run_{t}_rr1_btrfull_50m"
-echo === 3. RR2 + BTR net + SN 50M (5.5h) ===
-call :run_exe "run.$=%A5%>run.@hard250>run.@rr2>run.@munch>%RF%>%NET%>run.@envs64>run.@btreval_r2" "app.run_name=run_{t}_rr2_btrfull_50m"
-echo === 4. RR4 + BTR net + SN 50M, new eval (10.5h) ===
-call :run_exe "run.$=%A5%>run.@hard500>run.@rr4>run.@munch>%RF%>%NET%>run.@envs64>run.@btreval_r4" "app.run_name=run_{t}_rr4_btrfull_r2_50m"
-echo === 5. RR4 + Pool4 trunk SN 20M (4h) ===
-call :run_exe "run.$=%A5%>run.@hard500>run.@rr4>run.@munch>%RF%>run.@btrtrunkpool4>run.@envs64>run.@a5_20m>run.@btreval_r4" "app.run_name=run_{t}_rr4_btrtrunkpool4_20m"
-echo === 6. RR4 + Pool3 trunk SN 20M (4h) ===
-call :run_exe "run.$=%A5%>run.@hard500>run.@rr4>run.@munch>%RF%>run.@btrtrunkpool3>run.@envs64>run.@a5_20m>run.@btreval_r4" "app.run_name=run_{t}_rr4_btrtrunkpool3_20m"
+SET "S_R2=workspaces/atari-3rd/runs/run_20260914-143610_rr4_btrfull_r2_20m/agent_close.anet"
+SET "S_SN12=workspaces/atari-3rd/runs/run_20260915-155133_rr4_btrfull_sn12_20m/agent_close.anet"
+SET "S_AAI=workspaces/atari-3rd/runs/run_20260915-114835_rr4_btrfull_aainit_20m/agent_close.anet"
+SET "S_NOSN=workspaces/atari-3rd/runs/run_20260913-025232_rr4_btrnosn_20m/agent_close.anet"
+
+echo === 1. ev: btrfull r2 20M eps=0 (12m) ===
+call :run_exe "run.$=%A5%>run.@munch>%NET%>%EV%" "A3.auto_load_file=%S_R2%" "app.run_name=run_{t}_ev_btrfull_r2_20m"
+echo === 2. ev: sn12 20M eps=0 (12m) ===
+call :run_exe "run.$=%A5%>run.@munch>run.@btrnet>run.@btrsn12>%EV%" "A3.auto_load_file=%S_SN12%" "app.run_name=run_{t}_ev_btrfull_sn12_20m"
+echo === 3. ev: aainit 20M eps=0 (12m) ===
+call :run_exe "run.$=%A5%>run.@munch>%NET%>%EV%" "A3.auto_load_file=%S_AAI%" "app.run_name=run_{t}_ev_btrfull_aainit_20m"
+echo === 4. ev: btrnosn 20M eps=0 (12m) ===
+call :run_exe "run.$=%A5%>run.@munch>run.@btrnet>%EV%" "A3.auto_load_file=%S_NOSN%" "app.run_name=run_{t}_ev_btrnosn_20m"
+echo === 5. envs128 + replay_capacity 2M (lane 16,384) 50M (11h) ===
+call :run_exe "run.$=%A5%>%RR4%>%RF%>%NET%>run.@cap2m>run.@btreval_r4" "app.run_name=run_{t}_rr4_btrfull_envs128_cap2m_50m"
+echo === 6. walltime preset: RR1 + kyu-net 100M (5-6h) ===
+call :run_exe "run.$=%A5%>run.@hard125>run.@munch>%RF%>run.@to_100m>run.@btreval_r1" "app.run_name=run_{t}_rr1_va_hard125_100m"
 
 if "%FAILED_RUNS%"=="0" goto :all_succeeded
 echo === ALL DONE: %SUCCEEDED_RUNS% SUCCEEDED, %FAILED_RUNS% FAILED ===
